d4510fee53f8ea267386f829205ec261b325f538
Amit Kharod <kharodcloud1@gmail.com>
2021-12-27 20:06:54 +0530

    first commit

---
+++ package1/Base.java
@@ -0,0 +1,6 @@
+package package1;
+public class Base{
+    public void sayHi(String name){
+        System.out.println("hi " + name +"! How are you");
+    }
+}
---
+++ package2/Child.java
@@ -0,0 +1,9 @@
+package package2;
+import package1.*;
+
+public class Child {
+    public static void main(String args[]){
+        Base obj = new Base();
+        obj.sayHi("Amit");
+    }
+}
